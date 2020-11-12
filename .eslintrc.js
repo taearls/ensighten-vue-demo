3320b510f8f7d8f128a1bdb8f0d9bc608eaa8c66
Tyler Earls <tyler.earls@ensighten.com>
2020-11-12 16:13:54 -0600

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/essential",
+    "@vue/standard",
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "max-len": "off",
+    "no-tabs": ["error", { allowIndentationTabs: true }],
+    "vue/no-v-html": "off",
+    "comma-dangle": ["error", {
+      arrays: "always-multiline",
+      objects: "always-multiline",
+      imports: "never",
+      exports: "never",
+      functions: "never",
+    }],
+    "eol-last": ["error", "always"],
+    "object-curly-spacing": ["error", "always"],
+    "prefer-template": ["error"],
+    semi: ["error", "always"],
+    eqeqeq: ["error", "smart"],
+    quotes: [2, "double", { avoidEscape: true }],
+  },
+};
