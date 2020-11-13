e3148a37938081158cc664b578461daf9ad4ef62
Tyler Earls <tyler.earls@ensighten.com>
2020-11-13 16:16:52 -0600

    get v-if and v-show working in demo page

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <h1>This is a demo page</h1>
-    <input-toggle id="inputToggle" :external-toggled-prop="toggleProp" @input-toggled="toggleProp = $event" />
-    <p v-if="toggleProp">Now you see me</p>
+
+    <h2 class="text-lg font-bold">v if</h2>
+    <input-toggle id="vIfInputToggle" :external-toggled-prop="vIfToggleProp" @input-toggled="vIfToggleProp = $event" />
+    <pre>vIfToggleProp: {{vIfToggleProp}}</pre>
+    <p v-if="vIfToggleProp">Now you see me</p>
     <p v-else>Now I'm here instead</p>
+
+    <!-- TODO: use containers instead of br elements -->
+    <br/>
+
+    <h2 class="text-lg font-bold">v show</h2>
+    <input-toggle id="vShowInputToggle" :external-toggled-prop="vShowToggleProp" @input-toggled="vShowToggleProp = $event" />
+    <pre>vShowToggleProp: {{vShowToggleProp}}</pre>
+    <p v-if="vShowToggleProp">Now I have display block.</p>
+    <p v-else>Now I'm shown, and the other element has display none.</p>
   </div>
 </template>
 
@@ -12,7 +24,8 @@ import InputToggle from "@/components/InputToggle";
 export default {
   data() {
     return {
-      toggleProp: true,
+      vIfToggleProp: true,
+      vShowToggleProp: true,
     };
   },
   components: {
