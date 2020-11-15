51df73e3539ae8e6d0c12fc8a41b754ff51d36f5
Tyler Earls <tyler.a.earls@gmail.com>
2020-11-14 22:17:36 -0600

    fix webpack chunk name

--- src/router/index.js
+++ src/router/index.js
@@ -14,9 +14,9 @@ const routes = [
     path: "/demo",
     name: "Demo",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (demo.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Demo.vue"),
+    component: () => import(/* webpackChunkName: "demo" */ "../views/Demo.vue"),
   },
 ];
 
