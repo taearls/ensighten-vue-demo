1eb35587461b989b06b75d0916bda75081dd96d9
Tyler Earls <tyler.a.earls@gmail.com>
2020-11-14 22:12:19 -0600

    format components with prettier

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/demo">Demo</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -4,7 +4,11 @@
 
     <div class="my-4">
       <h2 class="text-lg font-bold">v-if</h2>
-      <input-toggle id="vIfInputToggle" :external-toggled-prop="vIfToggleProp" @input-toggled="vIfToggleProp = $event" />
+      <input-toggle
+        id="vIfInputToggle"
+        :external-toggled-prop="vIfToggleProp"
+        @input-toggled="vIfToggleProp = $event"
+      />
       <pre>vIfToggleProp: {{ vIfToggleProp }}</pre>
       <p v-if="vIfToggleProp">Now you see me</p>
       <p v-else>Now I'm here instead</p>
@@ -12,31 +16,57 @@
 
     <div class="my-4">
       <h2 class="text-lg font-bold">v-show</h2>
-      <input-toggle id="vShowInputToggle" :external-toggled-prop="vShowToggleProp" @input-toggled="vShowToggleProp = $event" />
+      <input-toggle
+        id="vShowInputToggle"
+        :external-toggled-prop="vShowToggleProp"
+        @input-toggled="vShowToggleProp = $event"
+      />
       <pre>vShowToggleProp: {{ vShowToggleProp }}</pre>
       <p v-show="vShowToggleProp">Now I have display block.</p>
-      <p v-show="!vShowToggleProp">Now I'm shown, and the original element has display none.</p>
+      <p v-show="!vShowToggleProp">
+        Now I'm shown, and the original element has display none.
+      </p>
     </div>
 
     <div class="my-4">
       <h2 class="text-lg font-bold">v-for</h2>
       <pre>foodList: {{ foodList }}</pre>
       <ul>
-        <div v-for="(food, index) in foodList" :key="'food-container-' + index" class="flex justify-center">
+        <div
+          v-for="(food, index) in foodList"
+          :key="'food-container-' + index"
+          class="flex justify-center"
+        >
           <li :key="'food-' + index">
             {{ food }}
           </li>
-          <button class="ml-2" :key="'remove-food-' + index" @click="foodList.splice(index, 1)">Remove Food</button>
+          <button
+            class="ml-2"
+            :key="'remove-food-' + index"
+            @click="foodList.splice(index, 1)"
+          >
+            Remove Food
+          </button>
         </div>
       </ul>
-
     </div>
 
     <div class="my-4">
       <h2 class="text-lg font-bold">v-model</h2>
       <pre>foodToAdd: "{{ foodToAdd }}"</pre>
-      <input class="block mx-auto my-2 p-2 focus:shadow-outline focus:outline-none border border-gray-800 rounded-md" type="text" placeholder="Add a Food to the list!" v-model.trim="foodToAdd">
-      <button class="add-food-button text-black bg-gray-300 px-4 py-2 rounded-md" @click="foodList.push(foodToAdd)" :disabled="foodToAdd === ''">Add Food to List</button>
+      <input
+        class="block mx-auto my-2 p-2 focus:shadow-outline focus:outline-none border border-gray-800 rounded-md"
+        type="text"
+        placeholder="Add a Food to the list!"
+        v-model.trim="foodToAdd"
+      />
+      <button
+        class="add-food-button text-black bg-gray-300 px-4 py-2 rounded-md"
+        @click="foodList.push(foodToAdd)"
+        :disabled="foodToAdd === ''"
+      >
+        Add Food to List
+      </button>
     </div>
   </div>
 </template>
